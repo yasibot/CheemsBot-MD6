b5fc02e54c22c9443220d8298b5e89bf0d526dee
Fastboosthub <108246545+Fastboosthub@users.noreply.github.com>
2023-01-23 10:13:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -48,7 +48,7 @@ global.mess = {
     group: 'Features Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This feature could be used by bot only',
-    wait: 'In process...',
+    wait: 'Jake is working on it...',
     linkm: 'Where is the link?',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',

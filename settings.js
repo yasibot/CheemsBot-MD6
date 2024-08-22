3eaa89c99fb7df6819c7069294767b432ddd2038
yasibot <150625478+yasibot@users.noreply.github.com>
2024-08-22 07:43:24 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 
 //documents variants
@@ -15,20 +15,21 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['2347043031692'] //ur owner number
-global.ownername = "🦄Teefundz" //ur owner name
-global.ytname = "YT: Fastboosthub" //ur yt chanel name
-global.socialm = "Instagram: Fastboosthub" //ur github or insta name
-global.location = "Ikorodu, Lagos, Nigeria" //ur location
+global.owner = ['94743143828'] //ur owner number
+global.ownername = "Yasintha" //ur owner name
+global.ytname = "YT: Yasiya Bus Kingdom" //ur yt chanel name
+global.socialm = "Instagram: yasi559" //ur github or insta name
+global.location = "Srilanka" //ur location
 
 //bot bomdy 
-global.ownernomer = "2347043031692" //ur number
-global.premium = ['2347043031692'] //ur premium number
-global.botname = 'Jake' //ur bot name
+global.ownernomer = "94715908319" //ur number
+global.premium = ['94743143828'] //ur premium number
+global.botname = 'Yasiya-WA-bot' //ur bot name
 global.linkz = "https://chat.whatsapp.com/Ei9otUjZS1j3dRx6dlwZx6" //your theme url which will be displayed on whatsapp
 global.websitex = "https://fastboosthub.com" //ur website to be displayed
 global.botscript = 'https://github.com/Fastboosthub/CheemsBot-MD6' //script link
-global.themeemoji = "🎖️" //ur theme emoji
+global.themeemoji = "🎖Owner avatar
+CheemsBot-MD" //ur theme emoji
 global.packname = "Teefundz" //ur sticker watermark packname
 global.author = "Teefundz bot\n\n\nA whatsapp bot developed by\n🎖️Teefundz\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +2347043031692" //ur sticker watermark author
 global.wm = "Fastboosthub Bot Inc." //ur watermark
